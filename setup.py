cbca8803dc2ca67e68b31d63d4091bc0466f7dac
Robert Binneman <r.binneman@gmail.com>
2019-03-06 19:10:03 +0200

    changed layout

--- setup.py
+++ setup.py
@@ -1,34 +1,29 @@
-import os
-from setuptools import find_packages, setup
-
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+from distutils.core import setup
 
 setup(
     name='revo_utils',
-    version='0.1',
-    packages=find_packages(),
-    include_package_data=True,
-    license='GNU LESSER GENERAL PUBLIC LICENSE v3',
+    version='0.1.0',
+    packages='revo_utils',
+    license='gpl-3.0',
     description='A collection of utilities which we use in all our projects',
-    long_description=README,
-    url='https://github.com/RobertBinneman/revo_utils/',
     author='Robert Binneman',
     author_email='r.binneman@gmail.com',
+    url='https://github.com/RobertBinneman/revo_utils/',
+    download_url='https://github.com/RobertBinneman/revo_utils/archive/0.1.0.tar.gz',
+    keywords=['UTILITIES'],
+    install_requires=[
+        'cryptography',
+        'djangorestframework',
+        'django',
+        'python-dateutil'
+    ],
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 2.1',  # replace "X.Y" as appropriate
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU LGPL v3',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        '3 - Alpha',
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: GNU Lesser General Public License v3.0',  # Again, pick a license
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
